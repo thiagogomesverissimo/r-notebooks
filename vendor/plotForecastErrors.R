043610d4611da18ec885a2048931db9e5127398c
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-11-10 22:57:22 -0300

    update

---
+++ vendor/plotForecastErrors.R
@@ -0,0 +1,30 @@
+plotForecastErrors <- function(forecastErrors)
+{
+  forecastErrorsSd <- sd(x = forecastErrors,
+                         na.rm = TRUE)
+  forecastErrorsMin <- min(forecastErrors,
+                           na.rm = TRUE) - forecastErrorsSd * 5
+  forecastErrorsMax <- max(forecastErrors,
+                           na.rm = TRUE) + forecastErrorsSd * 3
+  forecastErrorsNorm <- rnorm(n = 10000,
+                              mean = 0,
+                              sd = forecastErrorsSd)
+  binMin <- min(forecastErrorsMin, forecastErrorsNorm)
+  binMax <- max(forecastErrorsMax, forecastErrorsNorm)
+  binBreaks <- IQR(x = forecastErrors,
+                   na.rm = TRUE) / 4
+  bins <- seq(from = binMin,
+              to = binMax,
+              by = binBreaks)
+  hist(x = forecastErrors,
+       col = "#DCE319FF",
+       freq = FALSE,
+       breaks = bins)
+  with(data = hist(x = forecastErrorsNorm,
+                   plot = FALSE,
+                   breaks = bins),
+       expr = lines(x = mids,
+                    y = density,
+                    col = "#440154FF",
+                    lwd = 3))
+}
